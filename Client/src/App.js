8d41405a7516d7240e0a84058234f1c928c667d0
Will <wseguin@uwo.ca>
2024-01-31 16:08:26 -0500

    Navbar created, bootstrap style implemented

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
 import ContactPage from './components/ContactPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/contact" element={<ContactPage />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ Client/src/components/Header.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import './Header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom'
+function Header() {
+
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="/">
+                    <Link to='/'>
+                        CHEER
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link href='/contact'>
+                            <Link to='/contact'>
+                                Contact
+                            </Link>
+                        </Nav.Link>
+                        {/* <Nav.Link href='/public-lists'>
+                            <Link to='/public-lists'>
+                                Public Lists
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link href='/hero-search'>
+                            <Link to='/hero-search'>
+                                Search Heroes
+                            </Link>
+                        </Nav.Link>
+                        <NavDropdown title="More" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/my-profile">My Profile</NavDropdown.Item>
+                            <NavDropdown.Item
+                                href="/"
+                            >
+                                Logout
+                            </NavDropdown.Item>
+
+                        </NavDropdown>
+ */}
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
