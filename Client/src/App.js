3d86fd77ca9866aa639f52f8efffdde36c3a883c
Talia Megidish <tmegidis@uwo.ca>
2024-01-31 15:13:50 -0500

    added home page skeleton

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="First Page">
-      <header className="App-header">
-        test test 
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ Client/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>This is the home for our website.</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
+  
