f2607369e39fd71999e3a1974ee72bd77a7a2348
Talia Megidish <tmegidis@uwo.ca>
2024-01-31 15:18:25 -0500

    added contact page and route

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/contact" element={<ContactPage />} />
     </Routes>
     </BrowserRouter>
   );
---
+++ Client/src/components/ContactPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+      <div>
+        <h1>Contact Page</h1>
+        <p>This is the contact page for our website.</p>
+      </div>
+    );
+  };
+  
+  export default ContactPage;
+  
