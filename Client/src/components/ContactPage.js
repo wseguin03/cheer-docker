3d2dc2913b6b6ff513284fb248e32063f1f40bf3
sanjana adiga <sadiga@uwo.ca>
2024-01-31 15:33:59 -0500

    Added information to Home page and Contact page

--- Client/src/components/ContactPage.js
+++ Client/src/components/ContactPage.js
@@ -5,6 +5,9 @@ const ContactPage = () => {
       <div>
         <h1>Contact Page</h1>
         <p>This is the contact page for our website.</p>
+        <p>If you are interested in the CHEER Program, CHEER Works, or CHEER Connections, please contact our fearless leader, Ivey Hartman, at : ihartmancheer@gmail.com</p>
+        <p>Find us on Facebook at Family Connections Cheer</p><a href="https://www.facebook.com/familyconnectionscheers/" target></a>
+        <p>For more information please contact ihartmancheer@gmail.com</p>
       </div>
     );
   };
