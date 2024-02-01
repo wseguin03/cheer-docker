dea9411b06f41a65d87c0931cc421f1ffade6512
sanjana adiga <sadiga@uwo.ca>
2024-02-01 16:02:59 -0500

    added image in home page

--- Client/src/components/HomePage.js
+++ Client/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cheerLogo from '../assets/olli-cheer-logo.png'
 import { Container, Col } from 'react-bootstrap'
 
 const HomePage = () => {
@@ -8,7 +9,7 @@ const HomePage = () => {
         <h1>CHEER</h1>
         <p>Our Vision:</p>
         <p>To be a community of inclusion and a circle of friendship that supports and enhances the lives of our loved ones with intellectual disabilities as well as the whole family.</p>
-        <img src='\Client\src\assets\olli-cheer-logo.png'></img>
+        <img src={cheerLogo} alt="CHEER Logo" />
       </div>
     </Container>
   );
