c63351be74206d3c8ef6f8c9f7c3cf8099e462a4
Will <wseguin@uwo.ca>
2024-02-01 15:55:47 -0500

    Added homepage container

--- Client/src/components/HomePage.js
+++ Client/src/components/HomePage.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Container, Col } from 'react-bootstrap'
 
 const HomePage = () => {
-    return (
+  return (
+    <Container>
       <div>
         <h1>CHEER</h1>
         <p>Our Vision:</p>
         <p>To be a community of inclusion and a circle of friendship that supports and enhances the lives of our loved ones with intellectual disabilities as well as the whole family.</p>
         <img src='\Client\src\assets\olli-cheer-logo.png'></img>
       </div>
-    );
-  };
-  
-  export default HomePage;
-  
+    </Container>
+  );
+};
+
+export default HomePage;
