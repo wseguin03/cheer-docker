d434c57683392779b64282e2850cdba25b043139
sanjana adiga <sadiga@uwo.ca>
2024-02-01 16:53:40 -0500

    updated home page

--- Client/src/components/HomePage.js
+++ Client/src/components/HomePage.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import cheerLogo from '../assets/olli-cheer-logo.png'
-import { Container, Col } from 'react-bootstrap'
+import { Container, Col, Button,  Row, Card } from 'react-bootstrap';
+
 
 const HomePage = () => {
   return (
-    <Container>
-      <div>
-        <h1>CHEER</h1>
-        <p>Our Vision:</p>
-        <p>To be a community of inclusion and a circle of friendship that supports and enhances the lives of our loved ones with intellectual disabilities as well as the whole family.</p>
-        <img id="logo-img" src={cheerLogo} alt="CHEER Logo" />
-      </div>
-    </Container>
+    <Container className="caregiver-dashboard">
+    <Row className="text-center">
+        <Col>
+          <img id="logo-img" src={cheerLogo} alt="CHEER Logo" style={{ width: '10%' }} />
+        </Col>
+      </Row>
+      <Row className="text-center">
+        <Col>
+          <h1 className="my-4">CHEER Home</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Our Vision:</h2>
+          <p>
+            To be a community of inclusion and a circle of friendship that supports and enhances the lives of our loved ones with intellectual disabilities as well as the whole family.
+          </p>
+        </Col>
+      </Row>
+  </Container>
   );
 };
 
