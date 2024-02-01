976482dd9c52116dbca0eaac9500ee0cca4b2703
Talia Megidish <tmegidis@uwo.ca>
2024-02-01 15:53:36 -0500

    updated navigation bar

--- Client/src/components/Header.js
+++ Client/src/components/Header.js
@@ -10,39 +10,44 @@ function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/">
-                    <Link to='/'>
-                        Home
-                    </Link>
-                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav>
+                        <Nav.Link href='/'>
+                            <Link to='/'>
+                                Home
+                            </Link>
+                        </Nav.Link>
                         <Nav.Link href='/contact'>
                             <Link to='/contact'>
                                 Contact
                             </Link>
                         </Nav.Link>
-                        {/* <Nav.Link href='/public-lists'>
-                            <Link to='/public-lists'>
-                                Public Lists
+                        <Nav.Link href='/sponsors'>
+                            <Link to='/sponsors'>
+                                Sponsors
                             </Link>
                         </Nav.Link>
-                        <Nav.Link href='/hero-search'>
-                            <Link to='/hero-search'>
-                                Search Heroes
+                        <Nav.Link href='/client'>
+                            <Link to='/client'>
+                                Client
                             </Link>
                         </Nav.Link>
-                        <NavDropdown title="More" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/my-profile">My Profile</NavDropdown.Item>
-                            <NavDropdown.Item
-                                href="/"
-                            >
-                                Logout
-                            </NavDropdown.Item>
-
-                        </NavDropdown>
- */}
+                        <Nav.Link href='/caregiver'>
+                            <Link to='/caregiver'>
+                                Caregiver
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link href='/admin'>
+                            <Link to='/admin'>
+                                Admin
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link href='/login'>
+                            <Link to='/login'>
+                                Login
+                            </Link>
+                        </Nav.Link>
 
                     </Nav>
                 </Navbar.Collapse>
